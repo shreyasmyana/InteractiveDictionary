9e58a5238439459a57bd6b7bb5ec7c10e839be18
Shreyas <shreyasmyana1997@gmail.com>
2019-12-21 19:47:40 +0530

    Interactive Dictionary---v1

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
-print("Hello-World")
+import json
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+
+def translate(word):
+    if word in data:
+        print("DEFINATION:-")
+        for value in data[word]:
+            print(value)
+    else:
+        print("Unable to find word. Do you mean:",get_close_matches(word,data.keys())[0])
+        choice=input("YES / NO: ")
+        if choice.lower()=="yes":
+            translate(get_close_matches(word,data.keys())[0])
+        else:
+            print("Sorry try different word")
+
+
+if __name__ == "__main__":
+
+    word = input("Enter the word: ")
+    translate(word)
